6ec03c072257e2a79fab67593fcfd649e8c6ef2f
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-12-04 10:36:15 +0700

    feat: add category list

--- 09_pos_crud/pos/views.py
+++ 09_pos_crud/pos/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def category_list(request):
-    return render(request, 'category_list.html')
+    category1 = {
+        'code': 'C001',
+        'name' : 'Alat Berat'
+    }
+
+    context = {
+        'title': 'List Category',
+        'data1': 'Ini Data 1',
+        'data2': 'Ini Data 2',
+        'category1': category1,
+    }
+    return render(request, 'category_list.html', context)
