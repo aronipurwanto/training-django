1aac6e76b52f180f444a7c014fcdd38d5b9e21a7
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-12-04 11:10:06 +0700

    feat: add category list

--- 09_pos_crud/pos/urls.py
+++ 09_pos_crud/pos/urls.py
@@ -4,4 +4,5 @@ from pos import views
 
 urlpatterns = [
     path('category/', views.category_list, name='category_list'),
+    path('category/add', views.category_add, name='category_add'),
 ]
--- 09_pos_crud/pos/views.py
+++ 09_pos_crud/pos/views.py
@@ -1,16 +1,21 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
+
+from pos.models import Category
+
 
 # Create your views here.
 def category_list(request):
-    category1 = {
-        'code': 'C001',
-        'name' : 'Alat Berat'
-    }
-
+    data = Category.objects.all()
     context = {
-        'title': 'List Category',
-        'data1': 'Ini Data 1',
-        'data2': 'Ini Data 2',
-        'category1': category1,
+        'title':'Add Category',
+        'category_list': data,
     }
     return render(request, 'category_list.html', context)
+
+def category_add(request):
+    context = {
+        'title': 'Add Category',
+    }
+    return render(request, 'category_add.html', context)
