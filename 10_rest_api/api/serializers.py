7e30f25a0465adede26aa05ee9ca892214d94945
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-12-05 10:51:29 +0700

    feat: fix api

--- 10_rest_api/api/serializers.py
+++ 10_rest_api/api/serializers.py
@@ -8,32 +8,6 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'  # Atau, spesifikkan field yang ingin disertakan: ['id', 'code', 'name']
 
 class ProductSerializer(serializers.ModelSerializer):
-    category_id = serializers.PrimaryKeyRelatedField(
-        queryset=Category.objects.all(),
-        source='category',
-        write_only=True
-    )
-    category = serializers.StringRelatedField(read_only=True)  # Menampilkan nama kategori saat membaca data
-
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'category_id', 'price', 'stock', 'description']
-
-    def validate(self, data):
-        """
-        Validasi data secara keseluruhan.
-        """
-        if data['price'] <= 0:
-            raise serializers.ValidationError("Harga harus lebih besar dari 0.")
-        if data['stock'] < 0:
-            raise serializers.ValidationError("Stok tidak boleh negatif.")
-        # Tambahkan validasi lain sesuai kebutuhan
-        return data
-
-    def validate_category_id(self, value):
-        """
-        Validasi category_id.
-        """
-        if not Category.objects.filter(pk=value).exists():
-            raise serializers.ValidationError("Kategori tidak ditemukan.")
-        return value
+        fields = '__all__'
